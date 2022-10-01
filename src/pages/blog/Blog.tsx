feedf63b56cdf46acdeff735620a1bfc443c4528
Ospirentos <ospirentos@gmail.com>
2022-10-01 22:29:43 +0300

    remove warnings

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,8 +1,6 @@
 import SectionBox from "../../components/UI/SectionBox/SectionBox";
-import { useTranslation } from "react-i18next";
 
 const Blog: React.FC = () => {
-  const { t } = useTranslation();
   return (
     <SectionBox id="blog" title="BLOG">
       <article>
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,8 +1,6 @@
 import SectionBox from "../../components/UI/SectionBox/SectionBox";
-import { useTranslation } from "react-i18next";
 
 const Contact: React.FC = () => {
-  const { t } = useTranslation();
   return (
     <SectionBox id="contact" title="İLETİŞİM">
       <article>
