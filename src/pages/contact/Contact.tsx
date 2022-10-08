016523bf49231344206eb7062f2dfc3a16cbefba
Muhammet <isiyokm1993@gmail.com>
2022-10-09 01:15:07 +0300

    send email using emailjs localization on contact form

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,12 +1,9 @@
 import SectionBox from "../../components/UI/SectionBox/SectionBox";
-import ContactUsForm from "./ContactUsForm";
+import ContactUsForm from "../../components/ContactUsForm/ContactUsForm";
 
 const Contact: React.FC = () => {
   return (
     <SectionBox id="contact" title="İLETİŞİM">
-      <article>
-        <div className="text">Welcome to Contact page!</div>
-      </article>
       <ContactUsForm />
     </SectionBox>
   );
