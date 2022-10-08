f128a6957fcef6eddca8ec28d03cc68e7a4173d8
Muhammet <isiyokm1993@gmail.com>
2022-10-08 16:25:59 +0300

    EmailJS integration

--- src/pages/contact/ContactUsForm.js
+++ src/pages/contact/ContactUsForm.js
@@ -1,41 +1,72 @@
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import { useFormik } from "formik";
+import * as yup from "yup";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import emailjs from "@emailjs/browser";
 
 const validationSchema = yup.object({
   name: yup
-    .string('Enter your name')
+    .string("Enter your name")
     .min(2, "Too Short!")
     .max(50, "Too Long!")
-    .required('Name is required'),
+    .required("Name is required"),
   email: yup
-    .string('Enter your email')
-    .email('Enter a valid email')
-    .required('Email is required'),
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
   phoneNumber: yup
-    .string('Enter your phone number')
-    .required('Phone number is required')
+    .string("Enter your phone number")
+    .required("Phone number is required")
     .matches(
       /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/g,
-            "Invalid phone number"
-          ),
-  message: yup
-  .string('Enter your message')
-  .max(255, "Too Long!")
+      "Invalid phone number"
+    ),
+  message: yup.string("Enter your message").max(255, "Too Long!"),
 });
+
+const serviceId = "service_szot7ev";
+const templateId = "template_moqukoh";
+const publicKey = "OryHOGs6_QIKpX3Bz";
+
+const sendEmail = (values) => {
+  emailjs
+    .send(
+      serviceId,
+      templateId,
+      {
+        to_name: "Amber Clinic",
+        from_name: values.name,
+        message: values.message,
+        email: values.email,
+        phone_number: values.phoneNumber,
+        reply_to: "noreply",
+      },
+      publicKey
+    )
+    .then(
+      (result) => {
+        console.log("result: ", result);
+        console.log("values: ", values);
+        
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+};
 
 const ContactUsForm = () => {
   const formik = useFormik({
     initialValues: {
-      name: 'Deniz',
-      email: 'foobar@example.com',
-      phoneNumber: '+1 650 555 12 34',
-      message: '',
+      name: "Deniz",
+      email: "foobar@example.com",
+      phoneNumber: "+1 650 555 12 34",
+      message: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
+      sendEmail(values);
     },
   });
 
@@ -52,7 +83,7 @@ const ContactUsForm = () => {
           onBlur={formik.handleBlur}
           error={formik.touched.name && Boolean(formik.errors.name)}
           helperText={formik.touched.name && formik.errors.name}
-          sx={{marginTop: '1rem'}}
+          sx={{ marginTop: "1rem" }}
         />
         <TextField
           fullWidth
@@ -64,7 +95,7 @@ const ContactUsForm = () => {
           onBlur={formik.handleBlur}
           error={formik.touched.email && Boolean(formik.errors.email)}
           helperText={formik.touched.email && formik.errors.email}
-          sx={{marginTop: '1rem'}}
+          sx={{ marginTop: "1rem" }}
         />
         <TextField
           fullWidth
@@ -74,9 +105,11 @@ const ContactUsForm = () => {
           value={formik.values.phoneNumber}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
-          error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
+          error={
+            formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)
+          }
           helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
-          sx={{marginTop: '1rem'}}
+          sx={{ marginTop: "1rem" }}
         />
         <TextField
           multiline
@@ -91,9 +124,15 @@ const ContactUsForm = () => {
           helperText={formik.touched.message && formik.errors.message}
           rows={4}
           defaultValue="Enter a message..."
-          sx={{marginTop: '1rem'}}
+          sx={{ marginTop: "1rem" }}
         />
-        <Button color="primary" variant="contained" fullWidth type="submit" sx={{marginTop: '1rem'}}>
+        <Button
+          color="primary"
+          variant="contained"
+          fullWidth
+          type="submit"
+          sx={{ marginTop: "1rem" }}
+        >
           Submit
         </Button>
       </form>
@@ -101,4 +140,4 @@ const ContactUsForm = () => {
   );
 };
 
- export default ContactUsForm;
+export default ContactUsForm;
