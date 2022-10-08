501a5fb731787ac2a71e7e97cb13993c93c887e2
Zeynep Dündar <dundarzey@itu.edu.tr>
2022-10-08 18:50:51 +0300

    Style services

--- src/components/UI/ServiceCard/ServiceCard.tsx
+++ src/components/UI/ServiceCard/ServiceCard.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -29,29 +30,53 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
     () => navigate(`/our-services/${1} `),
     [navigate]
   );
-
+  // sx={{
+  //   maxWidth: "100%",
+  //   height: "100%",
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   border: "none",
+  //   boxShadow: "none",
+  // }}
   return (
-    <Card sx={{ maxWidth: 250, boxShadow: 1, borderRadius: 4, height: "100%" }}>
+    <Card
+      sx={{
+        maxWidth: "100%",
+        boxShadow: 1,
+        borderRadius: 2,
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <CardMedia
         component="img"
         height="140"
         image={require("../../../assets/images/services/" + imageName + ".png")}
         alt={name}
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardContent
+        sx={{
+          flexGrow: 1,
+        }}
+      >
+        <Typography gutterBottom variant="h5" component="div" color="primary">
           {name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {shortDesc}
         </Typography>
       </CardContent>
-      <CardActions sx={{ justifyContent: "center" }}>
+      <CardActions
+        sx={{ justifyContent: "left", display: "grid", paddingLeft: 2 }}
+      >
         <Button
-          variant="contained"
+          variant="outlined"
           size="small"
           color="primary"
+          endIcon={<NavigateNextIcon color="primary" sx={{ mr: 1 }} />}
           onClick={handleOnClick}
+          sx={{ textTransform: "none", borderColor: "primary" }}
         >
           Daha Fazlası
         </Button>
