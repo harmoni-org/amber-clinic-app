9a9d9876678bc342a6a5b20baa50b049e2f2f005
Ospirentos <ospirentos@gmail.com>
2022-10-09 13:43:05 +0300

    implement ContentRenderer component
    ContentRenderer component imports content.json to render requested fields.
    Currently, It can render dentist and service details.

---
+++ src/components/ContentRenderer/ContentRenderer.tsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react"
+import content from "./content.json";
+import { useTranslation } from "react-i18next";
+
+interface ContentRendererProps {
+    contentType: string;
+    id: string;
+}
+
+interface ContentJSON {
+    [index: string]: Content[]
+}
+
+interface Content {
+    id: string;
+    sections: Section[];
+}
+
+interface Section {
+    type: 'image' | 'header' | 'paragraph' | 'list';
+    content: string | string[];
+}
+
+const ContentRenderer = ({ id, contentType }: ContentRendererProps) => {
+    const [domContent, setDomContent] = useState<JSX.Element>(<></>)
+    const { i18n, t } = useTranslation();
+
+    useEffect(() => {
+        i18n.loadNamespaces(contentType, () => {
+            setDomContent(renderContent());
+        })
+    }, [])
+
+    const renderContent = (): JSX.Element => {
+        const contentStructure = (content as ContentJSON)[contentType];
+        const index = contentStructure.findIndex((section) => section.id === id);
+        const parsedStructure: JSX.Element[] = []
+
+        if (index > -1) {
+            const sections = contentStructure[index].sections;
+            let contentID = 0;
+
+            sections.forEach((section) => {
+                switch (section.type) {
+                    case "header":
+                        parsedStructure.push(<h1 key={contentID}>{t(`${id}.${section.content}`, { ns: contentType })}</h1>)
+                        break;
+                    case "paragraph":
+                        parsedStructure.push(<p key={contentID}>{t(`${id}.${section.content}`, { ns: contentType })}</p>)
+                        break;
+                    case "image":
+                        const image = require(`../../assets/images/dentists/${section.content}.png`)
+                        parsedStructure.push(<img key={contentID} src={image} />)
+                        break;
+                    case "list":
+                        parsedStructure.push(
+                            <ul>
+                                {(section.content as string[]).map(item => <li>{t(`${id}.${item}`, { ns: contentType })}</li>)}
+                            </ul>
+                        )
+                        break;
+                    default:
+                        break;
+                }
+                contentID++;
+            })
+        }
+        return <>{parsedStructure}</>;
+    }
+
+    return domContent;
+}
+
+export default ContentRenderer;
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,3 +3,4 @@ declare module "*.module.css";
 declare module "*.jpg";
 declare module "*.png";
 declare module "*.svg";
+declare module "*.json";
