9a79c32245b482f7ae36018be00fcee4641857e9
Zeynep Dündar <dundarzey@itu.edu.tr>
2022-10-10 21:14:07 +0300

    Add submenu to navbar

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    load: 'languageOnly',
-    ns: ['common'],
-    fallbackLng: 'en',
+    load: "languageOnly",
+    ns: ["common", "services"],
+    fallbackLng: "en",
     debug: true,
   });
 
