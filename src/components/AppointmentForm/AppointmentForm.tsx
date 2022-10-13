6a2e76ec7ff8bbf76edf6123b2ea353be94ae9ac
Zeynep Dündar <dundarzey@itu.edu.tr>
2022-10-14 01:08:42 +0300

    Style footer width

--- src/components/AppointmentForm/AppointmentForm.tsx
+++ src/components/AppointmentForm/AppointmentForm.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import SwipeableTemporaryDrawer from "../SwipeableTemporaryDrawer/SwipeableTemporaryDrawer";
 import ContactUsForm from "../ContactUsForm/ContactUsForm";
 import { Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const AppointmentForm = () => {
   const { t } = useTranslation();
@@ -14,6 +15,7 @@ const AppointmentForm = () => {
         <Typography variant="h5">
           {t("appointment_form_fields.ONLINE_APPOINTMENT")}
         </Typography>
+        <CloseIcon />
         <Typography variant="subtitle2">
           {t("appointment_form_fields.ONLINE_APkPOINTMENT_DESCRIPTION")}
         </Typography>
