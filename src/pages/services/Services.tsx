0146b43673135d38d6fa256d8dc2dc6e5aa32e8d
Ospirentos <ospirentos@gmail.com>
2022-11-03 23:03:09 +0300

    clean up services component and remove language filtering
    Since language filtering is handled by home component, this filtering
    is not required anymore.

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
 import SectionBox from "../../components/UI/SectionBox/SectionBox";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import ServiceCard from "../../components/UI/ServiceCard/ServiceCard";
-import Categories from "../../assets/settings/categories"
-import { useTranslation } from "react-i18next"
 
 interface Props {
   data: ServiceItem[];
@@ -20,18 +17,12 @@ export interface ServiceItem {
 }
 
 const Services = (props: Props) => {
-  const [services, setServices] = useState<ServiceItem[]>([]);
-  const {i18n} = useTranslation();
-
-  useEffect(() => {
-    setServices(props.data.filter((item) => item.categories.indexOf(Categories[i18n.language])))
-  }, [props.data])
 
   return (
     <SectionBox id="services" title="SERVICES">
       <Box sx={{ width: "90%" }}>
       <Grid container rowSpacing={3} columnSpacing={2} sx={{ width: "100%" }}>
-        {services.map((item) => (
+        {props.data.map((item) => (
           <Grid item xs={12} sm={6} md={3} key={item.id}>
             <ServiceCard key={item.id} item={item} />
           </Grid>
